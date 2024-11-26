73c737cd58aa2247714fae5b535cac2da50807c0
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 02:50:49 -0800

    Update classroom.py

--- backend/models/classroom.py
+++ backend/models/classroom.py
@@ -1,1 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Classroom(db.Model):
+    __tablename__ = 'classrooms'
+
+    class_id = db.Column(db.Integer, primary_key=True)
+    room_num = db.Column(db.String(50), nullable=False)
+    faculty_id = db.Column(db.Integer, nullable=False)  # Foreign key to Faculty table
+    course_id = db.Column(db.String(50), nullable=False)
+
+    def __init__(self,
 
