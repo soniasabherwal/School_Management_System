e1b9f00fdd8d79033e635b0e891e0950da98cb55
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 01:12:35 -0800

    Update StudentForm.js

--- frontend/scr/ components/Students/StudentForm.js
+++ frontend/scr/ components/Students/StudentForm.js
@@ -1,1 +1,28 @@
 
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+function AddClassroom() {
+  const [name, setName] = useState('');
+
+  const addClassroom = () => {
+    api.post('/classrooms', { name }).then(() => {
+      alert('Classroom added!');
+    });
+  };
+
+  return (
+    <div>
+      <h1>Add Classroom</h1>
+      <input
+        type="text"
+        placeholder="Classroom Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={addClassroom}>Add</button>
+    </div>
+  );
+}
+
+export default AddClassroom;
