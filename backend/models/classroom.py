58e9dea2ef2c75523e17385f010b90e3c501f486
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 02:51:33 -0800

    Update classroom.py

--- backend/models/classroom.py
+++ backend/models/classroom.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -10,5 +11,15 @@ class Classroom(db.Model):
     faculty_id = db.Column(db.Integer, nullable=False)  # Foreign key to Faculty table
     course_id = db.Column(db.String(50), nullable=False)
 
-    def __init__(self,
+    def __init__(self, room_num, faculty_id, course_id):
+        self.room_num = room_num
+        self.faculty_id = faculty_id
+        self.course_id = course_id
 
+    def to_dict(self):
+        return {
+            "class_id": self.class_id,
+            "room_num": self.room_num,
+            "faculty_id": self.faculty_id,
+            "course_id": self.course_id
+        }
