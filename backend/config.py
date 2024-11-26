c3c744ab150ff45a333b8c15327c69a01fcf9177
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 01:56:57 -0800

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,7 @@
 
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "app.db")}'  # SQLite database
+SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable modification tracking
