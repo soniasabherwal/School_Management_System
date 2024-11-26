909dbbbf41154670b12df1c03ee7034452c37810
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 03:02:45 -0800

    Update attendanceApi.js

--- frontend/api/attendanceApi.js
+++ frontend/api/attendanceApi.js
@@ -1,1 +1,16 @@
 
+const BASE_URL = "http://127.0.0.1:5000/attendance";
+
+export async function getAttendance() {
+    const response = await fetch(BASE_URL);
+    return response.json();
+}
+
+export async function addAttendance(data) {
+    const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return response.json();
+}
