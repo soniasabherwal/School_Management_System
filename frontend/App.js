83058cb35fddb02cdc3753f21af8ae21c12b585e
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 03:16:26 -0800

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,1 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import { AuthProvider } from "./useAuth";
+
+const App = () => (
+    <AuthProvider>
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    </AuthProvider>
+);
+
+export default App;
 
