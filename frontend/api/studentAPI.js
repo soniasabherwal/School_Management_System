23a8dffa2fc79b2a99888985d74f829413a68bf8
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 03:04:21 -0800

    Update studentAPI.js

--- frontend/api/studentAPI.js
+++ frontend/api/studentAPI.js
@@ -1,1 +1,21 @@
 
+const BASE_URL = "http://127.0.0.1:5000/students";
+
+export async function getStudents() {
+    const response = await fetch(BASE_URL);
+    return response.json();
+}
+
+export async function addStudent(data) {
+    const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return response.json();
+}
+
+export async function getStudentById(studentId) {
+    const response = await fetch(`${BASE_URL}/${studentId}`);
+    return response.json();
+}
