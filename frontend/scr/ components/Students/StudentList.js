3aa33d2dc5f35a9b647822201bbadee5a2077cbb
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 01:09:59 -0800

    Update StudentList.js

--- frontend/scr/ components/Students/StudentList.js
+++ frontend/scr/ components/Students/StudentList.js
@@ -1,1 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+
+function StudentList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    api.get('/students').then(response => setStudents(response.data));
+  }, []);
+
+  const deleteStudent = (id) => {
+    api.delete(`/students/${id}`).then(() => {
+      setStudents(students.filter(student => student.id !== id));
+    });
+  };
+
+  return (
+    <div>
+      <h1>Student List</h1>
+      <ul>
+        {students.map(student => (
+          <li key={student.id}>
+            {student.name}
+            <button onClick={() => deleteStudent(student.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default StudentList;
 
