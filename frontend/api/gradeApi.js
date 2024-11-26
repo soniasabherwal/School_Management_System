e3ffbc6ff7bb3ccc0c0c08f936ac07e24e86fe13
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 03:04:02 -0800

    Update gradeApi.js

--- frontend/api/gradeApi.js
+++ frontend/api/gradeApi.js
@@ -1,1 +1,16 @@
 
+const BASE_URL = "http://127.0.0.1:5000/grades";
+
+export async function getGrades() {
+    const response = await fetch(BASE_URL);
+    return response.json();
+}
+
+export async function addGrade(data) {
+    const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return response.json();
+}
