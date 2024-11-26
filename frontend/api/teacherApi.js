818a283fed04f7a29a1420ced82a01ab882de163
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 03:04:39 -0800

    Update teacherApi.js

--- frontend/api/teacherApi.js
+++ frontend/api/teacherApi.js
@@ -1,1 +1,16 @@
 
+const BASE_URL = "http://127.0.0.1:5000/teachers";
+
+export async function getTeachers() {
+    const response = await fetch(BASE_URL);
+    return response.json();
+}
+
+export async function addTeacher(data) {
+    const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return response.json();
+}
