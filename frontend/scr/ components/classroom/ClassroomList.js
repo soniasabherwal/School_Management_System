7d8eaec04ed844eba6379a2b98ef6b4f6273e23a
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 01:13:50 -0800

    Update ClassroomList.js

--- frontend/scr/ components/classroom/ClassroomList.js
+++ frontend/scr/ components/classroom/ClassroomList.js
@@ -1,1 +1,24 @@
 
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+
+function ClassroomList() {
+  const [classrooms, setClassrooms] = useState([]);
+
+  useEffect(() => {
+    api.get('/classrooms').then(response => setClassrooms(response.data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Classroom List</h1>
+      <ul>
+        {classrooms.map(classroom => (
+          <li key={classroom.id}>{classroom.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ClassroomList;
