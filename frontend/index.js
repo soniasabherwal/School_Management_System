f05eae3765de36eabc36caa51149b01e01582372
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 01:37:53 -0800

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+
+ReactDOM.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+  document.getElementById('root')
+);
