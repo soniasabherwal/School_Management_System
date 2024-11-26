64a8e67d1ae1ab67c2be1151b833d223dbd13f4d
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-26 03:03:10 -0800

    Update classroomAPI.js

--- frontend/api/classroomAPI.js
+++ frontend/api/classroomAPI.js
@@ -1,1 +1,16 @@
 
+const BASE_URL = "http://127.0.0.1:5000/classrooms";
+
+export async function getClassrooms() {
+    const response = await fetch(BASE_URL);
+    return response.json();
+}
+
+export async function addClassroom(data) {
+    const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return response.json();
+}
